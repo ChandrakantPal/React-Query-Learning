3edb643ff8544c70c10e8a4044864c555f257be0
ChandrakantPal <sanjay161097@gmail.com>
2021-01-22 19:22:45 +0530

    navbar imported with planets and persons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import Navbar from "./components/Navbar";
+import People from "./components/People";
+import Planets from "./components/Planets";
+
+const queryClient = new QueryClient();
 
 const App = () => {
-  return <div></div>;
+  const [page, setPage] = useState("planets");
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Star Wars Info</h1>
+        <Navbar setPage={setPage} />
+        <div className="content">
+          {page === "planets" ? <Planets /> : <People />}
+        </div>
+      </div>
+    </QueryClientProvider>
+  );
 };
 
 export default App;
